f9e786fb64404e311d7461c9efe9d78efc07b35e
Matt H <housems@hotmail.com>
2020-01-30 23:43:18 -0600

    Update HelloController.java

--- spring-boot/src/main/java/com/mattservice/springboot/HelloController.java
+++ spring-boot/src/main/java/com/mattservice/springboot/HelloController.java
@@ -20,35 +20,35 @@ public class HelloController {
         String password = "passord";
         String claimno = "poop";
         
-        Connection c = null;
-      try {
-         Class.forName("org.postgresql.Driver");
-         c = DriverManager
-            .getConnection("jdbc:postgresql://localhost:5432/claimnumber",
-            "user1", "passord");
-      } catch (Exception e) {
-         e.printStackTrace();
-         System.err.println(e.getClass().getName()+": "+e.getMessage());
-         System.exit(0);
-      }
-      System.out.println("Opened database successfully");
+//        Connection c = null;
+//      try {
+//         Class.forName("org.postgresql.Driver");
+//         c = DriverManager
+//            .getConnection("jdbc:postgresql://localhost:5432/claimnumber",
+//            "user1", "passord");
+//      } catch (Exception e) {
+//         e.printStackTrace();
+//         System.err.println(e.getClass().getName()+": "+e.getMessage());
+//         System.exit(0);
+//      }
+//      System.out.println("Opened database successfully");
    
         
         
-//         try (Connection con = DriverManager.getConnection(url, user, password);
-//                PreparedStatement pst = con.prepareStatement("SELECT VERSION()");
-//                ResultSet rs = pst.executeQuery()) {
-//
-//            if (rs.next()) {
-//                System.out.println(rs.getString(1));
-//                claimno = rs.getString(1);
-//            }
-//
-//        } catch (SQLException ex) {
-//
-//            Logger lgr = Logger.getLogger(HelloController.class.getName());
-//            lgr.log(Level.SEVERE, ex.getMessage(), ex);
-//        }
+         try (Connection con = DriverManager.getConnection(url, user, password);
+                PreparedStatement pst = con.prepareStatement("SELECT VERSION()");
+                ResultSet rs = pst.executeQuery()) {
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+                claimno = rs.getString(1);
+            }
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(HelloController.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
      
      return claimno;
     }
