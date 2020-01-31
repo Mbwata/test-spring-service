ceb6a48644f311567add599fb34c1a504472217f
Matt H <housems@hotmail.com>
2020-01-30 22:18:45 -0600

    try and connect to database

--- spring-boot/src/main/java/com/mattservice/springboot/HelloController.java
+++ spring-boot/src/main/java/com/mattservice/springboot/HelloController.java
@@ -2,13 +2,49 @@ package com.mattservice.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class HelloController {
 
+    public static String getClaimNo(){
+        
+        String url = "jdbc:postgresql://localhost:5432/claimnumber";
+        String user = "user1";
+        String password = "passord";
+        String claimno = "poop";
+        
+         try (Connection con = DriverManager.getConnection(url, user, password);
+                PreparedStatement pst = con.prepareStatement("SELECT VERSION()");
+                ResultSet rs = pst.executeQuery()) {
+
+            if (rs.next()) {
+                System.out.println(rs.getString(1));
+                claimno = rs.getString(1);
+            }
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(HelloController.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+     
+     return claimno;
+    }
+    
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        String claimno;
+        
+        claimno = getClaimNo();
+        
+        return claimno;
     }
 
 }
