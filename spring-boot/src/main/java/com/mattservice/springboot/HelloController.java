0133d8228bb531afe227bfc2bf67ca6e38c957c2
Matt H <housems@hotmail.com>
2020-02-07 15:20:46 -0600

    Update HelloController.java

--- spring-boot/src/main/java/com/mattservice/springboot/HelloController.java
+++ spring-boot/src/main/java/com/mattservice/springboot/HelloController.java
@@ -47,7 +47,7 @@ public class HelloController {
 
         
          try (Connection con = DriverManager.getConnection(url, user, password);
-                PreparedStatement pst1 = con.prepareStatement("select concat ('F-', cast(count(*)+1 as varchar),'-',translate (cast(current_date as varchar),'-','')) from claim_no_history where create_date > current_date");
+                PreparedStatement pst1 = con.prepareStatement("select concat ('W-', cast(count(*)+1 as varchar),'-',translate (cast(current_date as varchar),'-','')) from claim_no_history where create_date > current_date");
                 ResultSet rs1 = pst1.executeQuery()) {
 
             if (rs1.next()) {
