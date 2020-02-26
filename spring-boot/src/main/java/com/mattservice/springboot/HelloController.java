f492faa8ee7e7726a03c3c728cbee0650bc63c9a
Matt H <housems@hotmail.com>
2020-02-26 16:45:27 -0600

    try calling using a method

--- spring-boot/src/main/java/com/mattservice/springboot/HelloController.java
+++ spring-boot/src/main/java/com/mattservice/springboot/HelloController.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Properties;
-import java.io.FileInputStream; 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.InputStream;
@@ -18,8 +18,8 @@ import java.io.InputStream;
 @RestController
 public class HelloController {
 
-    public static String getClaimNo(){
-        
+    public static String getClaimNo() {
+
         //String url = "jdbc:postgresql://claim-db:5432/claimnumber";
         String url = "";
         String user = "user1";
@@ -27,38 +27,41 @@ public class HelloController {
         String claimno = "poop";
         String query = "insert into claim_no_history (claim_no) values( ? )";
 
-        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-        String dbConfigPath = rootPath + "connection.properties";
-   
-        try (InputStream input = new FileInputStream(dbConfigPath)) {
+//        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+//        String dbConfigPath = rootPath + "connection.properties";
+//        try (InputStream input = new FileInputStream(dbConfigPath)) {
+//
+//            Properties prop = new Properties();
+//
+//            // load a properties file
+//            prop.load(input);
+//
+//            // get the property value
+//            url = prop.getProperty("URL");
+//            user = prop.getProperty("Uname");
+//            password = prop.getProperty("password");
+//
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+        Properties prop = getDBconnection();
 
-            Properties prop = new Properties();
+        url = prop.getProperty("URL");
+        user = prop.getProperty("Uname");
+        password = prop.getProperty("password");
 
-            // load a properties file
-            prop.load(input);
+        try (Connection con = DriverManager.getConnection(url, user, password);
+                PreparedStatement pst3 = con.prepareStatement("delete from claim_no_history where claim_no = 'crap'")) {
 
-            // get the property value
-            url = prop.getProperty("URL");
-            user = prop.getProperty("Uname");
-            password = prop.getProperty("password");
-
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-  
-         try (Connection con = DriverManager.getConnection(url, user, password);
-             PreparedStatement pst3 = con.prepareStatement("delete from claim_no_history where claim_no = 'crap'")) {
-            
             pst3.executeUpdate();
 
         } catch (SQLException ex) {
 
             Logger lgr = Logger.getLogger(HelloController.class.getName());
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
-        }  
+        }
 
-        
-         try (Connection con = DriverManager.getConnection(url, user, password);
+        try (Connection con = DriverManager.getConnection(url, user, password);
                 PreparedStatement pst1 = con.prepareStatement("select concat ('W-', cast(count(*)+1 as varchar),'-',translate (cast(current_date as varchar),'-','')) from claim_no_history where create_date > current_date");
                 ResultSet rs1 = pst1.executeQuery()) {
 
@@ -73,11 +76,10 @@ public class HelloController {
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
             claimno = url;
         }
-     
-         try (Connection con = DriverManager.getConnection(url, user, password);
-             PreparedStatement pst2 = con.prepareStatement(query)) {
-            
-            
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+                PreparedStatement pst2 = con.prepareStatement(query)) {
+
             pst2.setString(1, claimno);
             pst2.executeUpdate();
 
@@ -86,23 +88,40 @@ public class HelloController {
             Logger lgr = Logger.getLogger(HelloController.class.getName());
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
         }
-                 
-     return claimno;
+
+        return claimno;
     }
-    
-    public static String gethealth(){
-    
+
+    public static Properties getDBconnection() {
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String dbConfigPath = rootPath + "connection.properties";
+        Properties prop = new Properties();
+        try (InputStream input = new FileInputStream(dbConfigPath)) {
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return prop;
+    }
+
+    public static String gethealth() {
+
         String healthdate = "Oh Hello!";
-                
-        return healthdate;      
+
+        return healthdate;
     }
-    
+
     @RequestMapping("/")
     public String index() {
         String claimno;
-        
+
         claimno = getClaimNo();
-        
+
         return claimno;
     }
 
@@ -110,8 +129,8 @@ public class HelloController {
     public String healthcheck() {
 
         String healthcheck = gethealth();
-        
+
         return healthcheck;
     }
-    
+
 }
